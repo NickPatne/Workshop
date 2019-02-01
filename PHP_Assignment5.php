1a708ebf33c9d954199ad7730bf17419c9f99c8e
NickPatne <46487327+NickPatne@users.noreply.github.com>
2019-02-01 09:29:23 +0530

    Add File

---
+++ PHP_Assignment5.php
@@ -0,0 +1,21 @@
+<style>
+body{
+    font-size:40px;
+    text-align:center;
+    margin-top:10%;
+}
+
+</style>
+<?php
+$current_file_name = basename($_SERVER['PHP_SELF']);
+$file_last_modified = filemtime($current_file_name);
+echo"File Name is: <b>".$current_file_name."</b>";
+echo"<br>";
+echo "Last modified <b> " . date("l, dS F, Y, h:ia", $file_last_modified)."</b>\n\n";
+
+
+$file = basename($_SERVER['PHP_SELF']); 
+$no_of_lines = count(file($file)); 
+echo"<br>";
+echo "There are<b>  ".$no_of_lines."</b>lines in<b>  ".$file."\n";
+?>
